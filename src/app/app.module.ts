fd885a9efcdd0474d57e82b5074da4196939517a
Jason <e0177109@comp.nus.edu.sg>
2020-12-11 11:48:51 +0800

    login done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ActivatedRoute, Router } from "@angular/router";
+import { SessionService } from '../../service/session.service';
+import { CustomerService } from '../../service/customer.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: any = null;
+  errorMessage: string = '';
+  loginError: boolean;
+
+  constructor(
+    public sessionService: SessionService,
+    public customerService: CustomerService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      password: new FormControl(this.sessionService.getPassword(), [
+        Validators.required,
+        Validators.minLength(0),
+        Validators.maxLength(64)
+      ]),
+      username: new FormControl(this.sessionService.getUsername(), [
+        Validators.required,
+        Validators.minLength(0),
+        Validators.maxLength(64)
+      ]),
+      rememberMe: new FormControl(this.sessionService.getRememberMe()),
+    }, { updateOn: 'blur' })
   }
 
+  login(): void {
+    console.log("submit form");
+
+    console.log(this.loginForm.value.username);
+    console.log(this.loginForm.value.password);
+
+
+    //Retrieve value from form
+    this.customerService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe(
+      response => {
+         console.log("response " + JSON.stringify(response));
+        if (response != null) {
+          if (this.loginForm.value.rememberMe == true) {
+            this.sessionService.setUsername(this.loginForm.value.username);
+            this.sessionService.setPassword(this.loginForm.value.password);
+            this.sessionService.setRememberMe(true);
+          } else {
+            this.sessionService.setUsername("");
+            this.sessionService.setPassword("");
+            this.sessionService.setRememberMe(false);
+          }
+          this.sessionService.setIsLogin(true);
+          this.sessionService.setAccessToken(response);
+          console.log(this.sessionService.getUsername());
+          console.log(this.sessionService.getAccessToken());
+          // this.sessionService.setUserId(response.userId);
+          //console.log(this.sessionService.getUserId());
+          this.loginError = false;
+
+          this.router.navigate(["/home"]);
+        }
+        else {
+          this.loginError = true;
+        }
+      },
+      error => {
+        this.loginError = true;
+        this.errorMessage = error
+      });
+  }
 }
