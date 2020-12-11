ad572fcb829c423d9d5c76e8274042ad8ef94c9f
BingBing1991 <dinoang_86@hotmail.com>
2020-12-11 13:37:20 +0800

    added error message for the view() for T-H component.ts

--- src/app/components/transaction-history/transaction-history.component.ts
+++ src/app/components/transaction-history/transaction-history.component.ts
@@ -14,6 +14,7 @@ export class TransactionHistoryComponent implements OnInit {
 
   transactionDetails: Transaction;
   customer: Customer;
+  errorMessage: string = '';
 
   constructor(public transactionService: TransactionViewService,
     public sessionService: SessionService,
@@ -40,6 +41,8 @@ export class TransactionHistoryComponent implements OnInit {
           this.transactionDetails.message = response.message;
           this.transactionDetails.payeeID = response.payeeID;
         }
-    })
-  }
+    },
+      error => {
+        this.errorMessage = error
+  })};
 }
