acb5ee3dbc5e866ee523a51c89cec1e6801da6cb
chingyeewai12 <chingyeewai12@gmail.com>
2020-12-11 13:39:53 +0800

    Added data binding for customer account details - Chingyee 1.39pm

--- src/app/components/view-account-details/view-account-details.component.ts
+++ src/app/components/view-account-details/view-account-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/class/customer';
+import { CustomerService } from '../../service/customer.service';
+import { SessionService } from '../../service/session.service';
 
 @Component({
   selector: 'app-view-account-details',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-account-details.component.scss']
 })
 export class ViewAccountDetailsComponent implements OnInit {
+  customer: Customer;
 
-  constructor() { }
+  constructor(public customerService: CustomerService,
+    public sessionService: SessionService) { 
+      this.customer = new Customer();
+    }
 
   ngOnInit(): void {
+    this.customer = JSON.parse(this.sessionService.getCustomer());
+    console.log("home page " + JSON.stringify(this.customer.firstName));
   }
 
 }
